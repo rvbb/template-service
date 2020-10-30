f6f87ca418bfecf4d98ba025cfa30db767068e53
namdq <namdq@fintech.smartosc.com>
2020-10-30 15:02:37 +0700

    update user entity name

--- src/main/java/com/smartosc/fintech/lms/entity/UserEntity.java
+++ src/main/java/com/smartosc/fintech/lms/entity/UserEntity.java
@@ -2,6 +2,7 @@ package com.smartosc.fintech.lms.entity;
 
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.Entity;
@@ -9,9 +10,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Entity(name = "user")
+@Entity(name = "users")
 public class UserEntity extends AuditEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
