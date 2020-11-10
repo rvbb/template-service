232b509e3ebf4a87e96dd761944449ef8c9a2b26
Dang Quoc Nam <namdq@fintech.smartosc.com>
2020-11-10 03:47:15 +0000

    Refactor User dto

--- src/main/java/com/smartosc/fintech/lms/dto/AuditDto.java
+++ src/main/java/com/smartosc/fintech/lms/dto/AuditDto.java
@@ -2,12 +2,14 @@ package com.smartosc.fintech.lms.dto;
 
 import lombok.Data;
 
+import javax.persistence.MappedSuperclass;
 import java.sql.Timestamp;
 
 @Data
+@MappedSuperclass
 public class AuditDto {
 
   private Timestamp createdDate;
 
-  private Timestamp lastModifiedDate;
+  private Timestamp lastUpdatedDate;
 }
