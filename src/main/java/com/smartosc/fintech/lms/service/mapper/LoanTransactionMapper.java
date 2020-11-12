04c409b62f489bfe701e59767c0b969f3c147a12
Nguyen Duy Minh <minhnd3@fintech.smartosc.com>
2020-11-12 09:43:47 +0700

    fix bug when repay loan

--- src/main/java/com/smartosc/fintech/lms/service/mapper/LoanTransactionMapper.java
+++ src/main/java/com/smartosc/fintech/lms/service/mapper/LoanTransactionMapper.java
@@ -2,8 +2,10 @@ package com.smartosc.fintech.lms.service.mapper;
 
 import com.smartosc.fintech.lms.dto.LoanTransactionDto;
 import com.smartosc.fintech.lms.entity.LoanTransactionEntity;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface LoanTransactionMapper {
 
     LoanTransactionMapper INSTANCE = Mappers.getMapper(LoanTransactionMapper.class);
