a6ca5cfc8d2b3ee5eaa1af85e5f394e369d26758
taitv <taitv@smartosc.com>
2020-11-13 10:20:16 +0700

    api list loan application

--- src/main/java/com/smartosc/fintech/lms/repository/LoanApplicationRepository.java
+++ src/main/java/com/smartosc/fintech/lms/repository/LoanApplicationRepository.java
@@ -2,7 +2,6 @@ package com.smartosc.fintech.lms.repository;
 
 import com.smartosc.fintech.lms.entity.LoanApplicationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +11,7 @@ import java.util.Optional;
 public interface LoanApplicationRepository extends JpaRepository<LoanApplicationEntity, Long> {
     Optional<LoanApplicationEntity> findLoanApplicationEntityByUuid(String uuid);
 
-    @Query(value = "SELECT * FROM `lms-service`.loan_application where user_id=?1", nativeQuery = true)
-    List<LoanApplicationEntity> findLoanApplicationByUser(long userId);
+//    @Query(value = "SELECT * FROM `lms-service`.loan_application where user_id=?1", nativeQuery = true)
+    List<LoanApplicationEntity> findLoanApplicationEntityByUserId(long userId);
 
 }
