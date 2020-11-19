746207cf7ba73bc531f29bb99e34eadbf78e1062
taitv <taitv@smartosc.com>
2020-11-19 11:26:44 +0700

    update util

--- src/main/java/com/smartosc/fintech/lms/repository/RepaymentRepository.java
+++ src/main/java/com/smartosc/fintech/lms/repository/RepaymentRepository.java
@@ -2,7 +2,6 @@ package com.smartosc.fintech.lms.repository;
 
 import com.smartosc.fintech.lms.entity.RepaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +10,6 @@ import java.util.Optional;
 @Repository
 public interface RepaymentRepository extends JpaRepository<RepaymentEntity, Long> {
     Optional<RepaymentEntity> findFirstByUuid(String uuid);
-
-    @Query(value = "SELECT * FROM `lms-service`.repayment where loan_application_key=?1 order by due_date desc;", nativeQuery = true)
-    List<RepaymentEntity> findRepaymentOfLoanOrderByDuedate(String loanKey);
 
     List<RepaymentEntity> findByLoanApplicationUuidOrderByDueDateDesc(String loanKey);
 
