f35ac229cc7eb820e67c37672092bf81709bf32d
taitv <taitv@smartosc.com>
2020-11-25 17:08:40 +0700

    LMS_118

--- src/main/java/com/smartosc/fintech/lms/repository/LoanApplicationRepository.java
+++ src/main/java/com/smartosc/fintech/lms/repository/LoanApplicationRepository.java
@@ -12,14 +12,14 @@ import java.util.Optional;
 public interface LoanApplicationRepository extends JpaRepository<LoanApplicationEntity, Long> {
     Optional<LoanApplicationEntity> findLoanApplicationEntityByUuid(String uuid);
 
-    @Query(value = "SELECT *, case status\n" +
-            "when  4 then 1\n" +
-            "when  3 then 2\n" +
-            "when  1 then 3\n" +
-            "when  5 then 4\n" +
-            "when  2 then 5\n" +
+    @Query(value = "select *, case status\n" +
+            "when 4 then 1\n" +
+            "when 3 then 2\n" +
+            "when 1 then 3\n" +
+            "when 5 then 4\n" +
+            "when 2 then 5\n" +
             "end as temp\n" +
-            "FROM `lms-service`.loan_application\n" +
+            "from `lms-service`.loan_application\n" +
             "where status!=0\n" +
             "and user_id=?1\n" +
             "order by temp asc;", nativeQuery = true)
