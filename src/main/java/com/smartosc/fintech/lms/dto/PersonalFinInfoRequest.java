3c3bc865f824b0297c9397374313ab6df3a03409
Nguyen Van Hoang <hoangnv2@fintech.smartosc.com>
2020-12-15 18:08:30 +0700

    lms-184: recorrect request object validation

--- src/main/java/com/smartosc/fintech/lms/dto/PersonalFinInfoRequest.java
+++ src/main/java/com/smartosc/fintech/lms/dto/PersonalFinInfoRequest.java
@@ -12,15 +12,18 @@ import javax.validation.constraints.Size;
 public class PersonalFinInfoRequest {
 
     @Size(max = 15)
+    @NotBlank
     private String preTaxIncome;
 
     @Size(max = 150, min = 15)
     @NotBlank
-    private String company;
+    private String companyName;
 
     @Size(max = 150, min = 15)
+    @NotBlank
     private String companyAddress;
 
     @Size(max = 15)
+    @NotBlank
     private String expense;
 }
