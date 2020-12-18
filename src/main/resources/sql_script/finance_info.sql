5235f095cd7e3aec82809e0fd9c6e846129cf806
Nguyen Van Hoang <hoangnv2@fintech.smartosc.com>
2020-12-18 18:53:11 +0700

    add sql script

--- src/main/resources/sql_script/finance_info.sql
+++ src/main/resources/sql_script/finance_info.sql
@@ -5,12 +5,13 @@
 -- DROP TABLE public.finance_info;
 
 CREATE TABLE public.finance_info (
-id int8 NOT NULL,
-company_address varchar(500) NOT NULL,
-company_name timestamp NOT NULL,
-loan_application_id int4 NOT NULL,
-expense float8 NOT NULL,
-last_update date NULL,
-pre_tax_income float8 NOT NULL,
-CONSTRAINT banklist_demo_pkey PRIMARY KEY (id)
+	id int8 NOT NULL,
+	company_address varchar(500) NOT NULL,
+	company_name varchar(300) NOT NULL,
+	expense float8 NOT NULL,
+	last_update date NULL,
+	pre_tax_income float8 NOT NULL,
+	uuid varchar(24) NULL,
+	status int4 NULL,
+	CONSTRAINT banklist_demo_pkey PRIMARY KEY (id)
 );
